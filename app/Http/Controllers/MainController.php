fdd961d6c62a8c19b95b88bc449c2caee0656dd1
Ivan Pytlyak <mario38671@gmail.com>
2023-04-20 22:51:15 +0300

    20.04.2023

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,9 +8,35 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::get();
+        $productsQuery = Product::query();
+
+        if ($request->filled('hit')) {
+            $productsQuery->where('hit', '=', '1');
+        }
+        if ($request->filled('new')) {
+            $productsQuery->where('new', '=', '1');
+        }
+        if ($request->filled('recommend')) {
+            $productsQuery->where('recommend', '=', '1');
+        }
+
+        // if ($request->has('recommend')) { // аналог
+        //     $productsQuery->where('recommend', 1);
+        // }
+
+        if ($request->filled('price_from')) {
+            $productsQuery->where('price', '>=', $request->price_from); //фильтр условие
+        }
+        if ($request->filled('price_to')) {
+            $productsQuery->where('price', '<=', $request->price_to); //фильтр условие
+        }
+
+        $products = $productsQuery->paginate(6); //->withPath("?" . $request->getQueryString()); // =get() с фиксированным / withPath позволяет переносить значения фильтров на другую страницу путем добавления пагинации страниц в get запрос. ->getQueryString() дефолтный метод
+        // $urlWithFilters = url()->current() . '?' . http_build_query($request->except('page')); // http_build_query — Генерирует URL-кодированную строку запроса // Метод except возвращает все элементы коллекции, кроме тех, у которых есть указанные ключи:
+        // $products->withPath($urlWithFilters);
+        // $products->withPath("?" . $request->getQueryString());
         return view('index', compact('products'));
     }
 
